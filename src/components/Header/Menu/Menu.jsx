b0b47bf3f279bc656639026ecec3f28a1677f0ea
ArtemProgrammist <artemis09035@gmail.com>
2020-06-06 12:20:14 +0300

    Done:  -Background fixed

--- src/components/Header/Menu/Menu.jsx
+++ src/components/Header/Menu/Menu.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import classes from './Menu.module.css'
 import {NavLink} from "react-router-dom";
+import {setBurgerStatus} from "../../../redux/header-reducer";
 
 const Menu = (props) => {
     if (props.status){
         return (
             <div className={classes.menu}>
                 <nav className={classes.nav}>
-                    <NavLink to={'/Courses'} className={classes.nav__link}>Courses list</NavLink>
-                    <NavLink to={'/Calculator'} className={classes.nav__link}>Exchange calculator</NavLink>
-                    <NavLink to={'/Currency'} className={classes.nav__link}>Some link</NavLink>
-                    <NavLink to={'/Currency'} className={classes.nav__link}>Some link</NavLink>
+                    <NavLink to={'/Courses'} onClick={ () => props.setBurgerStatus(false)} className={classes.nav__link}>Courses list</NavLink>
+                    <NavLink to={'/Calculator'} onClick={ () => props.setBurgerStatus(false)} className={classes.nav__link}>Exchange calculator</NavLink>
+                    <NavLink to={'/Currency'} onClick={ () => props.setBurgerStatus(false)} className={classes.nav__link}>Some link</NavLink>
+                    <NavLink to={'/Currency'} onClick={ () => props.setBurgerStatus(false)} className={classes.nav__link}>Some link</NavLink>
                 </nav>
             </div>
         )
