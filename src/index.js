a4b1518dea56249c670ada51e787b5b612eee7cc
ArtemProgrammist <artemis09035@gmail.com>
2020-06-14 12:06:26 +0300

    Done: Changed BrowserRouter to HashRouter (For github pages)

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import store from "./redux/redux-store";
 
 
 
 ReactDOM.render(
-    <BrowserRouter basename={process.env.PIBLIC_URL}>
+    <HashRouter basename={process.env.PIBLIC_URL}>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root')
 );
