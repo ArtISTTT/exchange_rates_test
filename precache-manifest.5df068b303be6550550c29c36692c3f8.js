62b9c417e45bd55165bc049aeca2ec88858d4edd
ArtemProgrammist <artemis09035@gmail.com>
2020-06-14 11:45:51 +0300

    Updates

---
+++ precache-manifest.5df068b303be6550550c29c36692c3f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a5ad97c8f26113d4092ae9f82eeb906",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a600d115cc43ad63263",
+    "url": "/static/css/main.51e25f40.chunk.css"
+  },
+  {
+    "revision": "58298e1fdcce0f49fa3f",
+    "url": "/static/js/2.c11ee789.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.c11ee789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a600d115cc43ad63263",
+    "url": "/static/js/main.790ee7dc.chunk.js"
+  },
+  {
+    "revision": "ac6277282b577dedc481",
+    "url": "/static/js/runtime-main.c2b172ac.js"
+  },
+  {
+    "revision": "ee75e1e9b25572eb26294fea97e307c0",
+    "url": "/static/media/background.ee75e1e9.png"
+  },
+  {
+    "revision": "215d13f9e0ebcd787cf3040d379d34dd",
+    "url": "/static/media/preloader_2.215d13f9.svg"
+  }
+]);
