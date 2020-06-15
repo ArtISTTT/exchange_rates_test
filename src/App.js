7038fc7a619004af780eff03c1975300508dc8d6
ArtemProgrammist <artemis09035@gmail.com>
2020-06-15 14:45:10 +0300

    Done: Added Redirect to courses from homepage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Route} from "react-router-dom"
+import {Route, Switch, Redirect} from "react-router-dom"
 import {connect} from "react-redux";
 import {initializeApp} from "./redux/app-reducer";
 import CurrencyContainer from "./components/Currency/CurrencyContainer";
@@ -14,44 +14,48 @@ import MenuContainer from "./components/Header/Menu/MenuContainer";
 
 class App extends React.Component {
 
-  componentDidMount() {
-    this.props.initializeApp(this.props.currency)
+    componentDidMount() {
+        this.props.initializeApp(this.props.currency)
 
-    if (JSON.parse(localStorage.getItem('pairsData')) === null) {
-      localStorage.setItem("pairsData", JSON.stringify(this.props.pairsData));
+        if (JSON.parse(localStorage.getItem('pairsData')) === null) {
+            localStorage.setItem("pairsData", JSON.stringify(this.props.pairsData));
+        }
     }
-  }
 
-  render() {
-    if (!this.props.initialized) {
-      return <Preloader />
-    }
-    return (
+    render() {
+        if (!this.props.initialized) {
+            return <Preloader/>
+        }
+        return (
 
-        <div className="App">
-          <div className="container">
-            <div className="app-wrapper">
-              <HeaderContainer/>
-              <MenuContainer />
-              <div className="app-wrapper-content">
-                <Route path='/courses' render={() => <CurrencyContainer/>}/>
-                <Route path='/calculator' render={() => <CalculatorContainer/>}/>
-              </div>
+            <div className="App">
+                <div className="container">
+                    <div className="app-wrapper">
+                        <HeaderContainer/>
+                        <MenuContainer/>
+                        <div className="app-wrapper-content">
+                            <Switch>
+                                <Route exact path='/' render={() => <Redirect to="/courses"/>}/>
+                                <Route path='/courses' render={() => <CurrencyContainer/>}/>
+                                <Route path='/calculator' render={() => <CalculatorContainer/>}/>
+                            </Switch>
+
+                        </div>
+
+                    </div>
+                </div>
 
             </div>
-          </div>
 
-        </div>
-
-    );
-  }
+        );
+    }
 }
 
 const mapStateToProps = (state) => ({
-  initialized: state.app.initialized,
-  currency: state.currencyPage.currency,
-  pairsData: state.currencyPage.pairsData,
-  statusBurger: state.header.status
+    initialized: state.app.initialized,
+    currency: state.currencyPage.currency,
+    pairsData: state.currencyPage.pairsData,
+    statusBurger: state.header.status
 })
 
 export default connect(mapStateToProps, {initializeApp, setLocalStoragePairsData, setBurgerStatus})(App)
